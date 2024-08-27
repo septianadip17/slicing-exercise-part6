2766990829ea0e21d8f7d2928e22e5a046361d6a
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 11:33:49 +0700

    members added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Members from "./pages/Members";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="members" element={<Members />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
